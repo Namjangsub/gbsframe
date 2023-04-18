b8d222a41bb67347553365439543a616001362ba
choinchan <c@ccccc.io>
2023-04-18 16:59:25 +0900

    수주등록(파일트리 완료전)

--- src/main/java/com/dksys/biz/user/cr/cr02/service/CR02Svc.java
+++ src/main/java/com/dksys/biz/user/cr/cr02/service/CR02Svc.java
@@ -1,5 +1,7 @@
 package com.dksys.biz.user.cr.cr02.service;
 
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +12,15 @@ public interface CR02Svc {
 	List<Map<String, Object>> selectOrdrsList(Map<String, String> param);
 
 	Map<String, Object> selectOrdrsInfo(Map<String, String> paramMap);
+
+	String selectMaxOrdrsNo(Map<String, String> param);
+
+	void insertOrdrs(Map<String, String> param, MultipartHttpServletRequest mRequest);
+	void updateOrdrs(Map<String, String> param);
+
+
+
+
 
 
 }
--- src/main/java/com/dksys/biz/user/cr/cr02/service/impl/CR02Svcmpl.java
+++ src/main/java/com/dksys/biz/user/cr/cr02/service/impl/CR02Svcmpl.java
@@ -4,8 +4,7 @@ import com.dksys.biz.admin.cm.cm08.service.CM08Svc;
 import com.dksys.biz.user.cr.cr01.mapper.CR01Mapper;
 import com.dksys.biz.user.cr.cr02.mapper.CR02Mapper;
 import com.dksys.biz.user.cr.cr02.service.CR02Svc;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,13 +39,98 @@ public class CR02Svcmpl implements CR02Svc {
 
 
 		Map<String, Object> ordrsInfo = cr02Mapper.selectOrdrsInfo(paramMap);
-		List<Map<String, Object>> ordrsPaymentPlan = cr02Mapper.selectPmntPlan(paramMap);
-		ordrsInfo.put("list", ordrsPaymentPlan);
+
 
 
 		return ordrsInfo;
 
 
+	}
+	@Override
+	public String selectMaxOrdrsNo(Map<String, String> param) {
+		return cr02Mapper.selectMaxOrdrsNo(param);
+	}
+	@Override
+	public void insertOrdrs(Map<String, String> param,MultipartHttpServletRequest mRequest){
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+		Type mapList = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+
+
+		cr02Mapper.insertOrdrs(param);
+		System.out.println("리스트보기"+param.get("planArr"));
+		System.out.println("제거후"+removeEmptyObjects(param.get("planArr")));
+
+
+
+		List<Map<String, String>> planArr = gson.fromJson(removeEmptyObjects(param.get("planArr")), mapList);
+
+		for (Map<String, String> planMap : planArr) {
+
+			planMap.put("coCd", param.get("coCd"));
+			planMap.put("ordrsNo", param.get("ordrsNo"));
+			planMap.put("estNo", param.get("estNo"));
+			planMap.put("userId", param.get("userId"));
+			planMap.put("pgmId", param.get("pgmId"));
+			planMap.put("currUnit", param.get("currUnit"));
+
+			cr02Mapper.insertClmnPlanHis(planMap);
+			cr02Mapper.insertClmnPlan(planMap);
+		}
+
+		List<Map<String, String>> detailArr = gson.fromJson(removeEmptyObjects(param.get("detailArr")), mapList);
+
+		for (Map<String, String> detailMap : detailArr) {
+
+			detailMap.put("coCd", param.get("coCd"));
+			detailMap.put("ordrsNo", param.get("ordrsNo"));
+			detailMap.put("estNo", param.get("estNo"));
+			detailMap.put("userId", param.get("userId"));
+			detailMap.put("pgmId", param.get("pgmId"));
+			detailMap.put("currUnit", param.get("currUnit"));
+
+			cr02Mapper.insertOrdrsDetail(detailMap);
+		}
+
+
+
+
+
+
+
+	}
+
+	@Override
+	public void updateOrdrs(Map<String, String> param){
+
+		cr02Mapper.insertOrdrs(param);
+		cr02Mapper.insertOrdrsDetail(param);
+		cr02Mapper.insertClmnPlan(param);
+
+
+	}
+
+	public static String removeEmptyObjects(String jsonArrayString) {
+		if (jsonArrayString == null || !jsonArrayString.startsWith("[") || !jsonArrayString.endsWith("]")) {
+			return jsonArrayString;
+		}
+
+		JsonParser parser = new JsonParser();
+		JsonArray jsonArray = parser.parse(jsonArrayString).getAsJsonArray();
+		JsonArray filteredJsonArray = new JsonArray();
+
+		for (JsonElement jsonElement : jsonArray) {
+			if (jsonElement.isJsonObject()) {
+				JsonObject jsonObject = jsonElement.getAsJsonObject();
+				if (!jsonObject.entrySet().isEmpty()) {
+					filteredJsonArray.add(jsonObject);
+				}
+			} else if (!jsonElement.isJsonNull()) {
+				filteredJsonArray.add(jsonElement);
+			}
+		}
+
+		Gson gson = new Gson();
+		return gson.toJson(filteredJsonArray);
 	}
 
 
