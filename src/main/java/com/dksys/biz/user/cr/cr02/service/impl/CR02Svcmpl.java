48433609ef87a9351af445490a5bb8090a320ce5
choinchan <c@ccccc.io>
2023-04-25 18:49:33 +0900

    파일트리 수정 및 수주 캘린더등 수정

--- src/main/java/com/dksys/biz/user/cr/cr02/service/impl/CR02Svcmpl.java
+++ src/main/java/com/dksys/biz/user/cr/cr02/service/impl/CR02Svcmpl.java
@@ -51,7 +51,7 @@ public class CR02Svcmpl implements CR02Svc {
 	public void insertOrdrs(Map<String, String> param,MultipartHttpServletRequest mRequest){
 		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 		Type mapList = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
-		param.put("fileTrgtKey",selectMaxOrdrsNo(param));
+		param.put("ordrsNo",selectMaxOrdrsNo(param));
 		cr02Mapper.insertOrdrs(param);
 		List<Map<String, String>> planArr = gson.fromJson(removeEmptyObjects(param.get("planArr")), mapList);
 		for (Map<String, String> planMap : planArr) {
@@ -83,12 +83,10 @@ public class CR02Svcmpl implements CR02Svc {
 		for (int i = 0; i < mRequest.getFiles("files").size(); i++) {
 			String nodeIdKey = "nodeId_" + i;
 			String nodeId = mRequest.getParameter(nodeIdKey);
-
 			// 각 파일에 대한 fileTrgtKey를 가져옵니다.
 			String fileTrgtTyp = mRequest.getParameter("fileTrgtTyp_" + i);
-
 			// 각 파일에 대해 uploadFile 메소드를 호출하며, nodeId와 fileTrgtKey를 인자로 전달합니다.
-			cm08Svc.uploadFile(fileTrgtTyp, param.get("fileTrgtKey"), mRequest, nodeId);
+			cm08Svc.uploadFile(fileTrgtTyp, param.get("ordrsNo"), mRequest, nodeId);
 
 		}
 
--- src/main/resources/static/js/fileTree.js
+++ src/main/resources/static/js/fileTree.js
@@ -114,19 +114,41 @@ var treeModule = (function () {
                 // 선택한 노드의 모든 하위 노드를 가져옵니다.
                 var childrenNodes = getAllChildrenNodes(data.instance, clickedId);
 
-                // 선택한 노드와 하위 노드에 관련된 파일 목록을 불러옵니다.
-                var allNodes = [clickedId].concat(childrenNodes);
-                getAllFilesForNodes(allNodes, function (allFiles) {
 
-                    fileTreeGridView.reqSetData(allFiles);
-                });
+                selectedNodeId = data.node.id;
+                isEditMode= paramObj.actionType ==='U'
+                if (isEditMode) {
+                    var allNodes = [clickedId].concat(childrenNodes);
+                    // 수정 화면인 경우, 데이터베이스에서 파일 목록을 가져옵니다.
+                    getAllFilesForNodes(allNodes, function (allFiles) {
+                        fileTreeGridView.reqSetData(allFiles);
+                    });
+                } else {
+                    //표시하는 부분
+                    var relatedFiles = fileArr.filter(function (file) {
+                        return file.nodeId === selectedNodeId;
+                    });
+                    fileTreeGridView.reqSetData(relatedFiles);
+
+                }
+
+
+
+
+
 
                 if (!$('#'+selector).is(":visible")) {
                     $('#'+selector).show();
                 }
 
+
                 // 합쳐진 코드 부분 시작
-                const buttonFile = document.getElementById("button_file");
+                // const buttonFile = document.getElementById("button_file");
+                // document.getElementById('zip_down').addEventListener('click', function() {
+                //     downloadAllFilesInGrid();
+                // });
+
+
                 if (data.node.id.length < 7 || data.node.id=="FILETREE") {
 
                     buttonFile.disabled = true; // 비활성화
@@ -136,8 +158,6 @@ var treeModule = (function () {
                     buttonFile.style.backgroundColor = ""; // 기본 배경색으로 변경 (옵션)
 
                 }
-                selectedNodeId = data.node.id;
-
 
                 if (fileTrgtTypMapping.hasOwnProperty(selectedNodeId)) {
                     fileTrgtTyp = fileTrgtTypMapping[selectedNodeId];
@@ -145,12 +165,9 @@ var treeModule = (function () {
                     // 선택한 노드에 대한 처리가 필요 없는 경우 여기에 default 처리를 추가하세요.
                 }
 
-                //표시하는 부분
-                var relatedFiles = fileArr.filter(function (file) {
-                    return file.nodeId === selectedNodeId;
-                });
+
                 $("#file_tit").html($('#'+selector).jstree('get_selected', true)[0].text);
-                fileTreeGridView.reqSetData(relatedFiles);
+
                 // 합쳐진 코드 부분 끝
             })
             .on('open_node.jstree', function (e, data) {
@@ -278,3 +295,65 @@ function downloadFile(fileKey) {
         location.href = "/admin/cm/cm08/fileDownload?filePath=" + filePath;
     });
 }
+
+function downloadAllFiles(fileKeys) {
+    if (fileKeys.length === 0) return;
+
+    var fileKey = fileKeys.shift();
+    downloadFile(fileKey);
+
+    setTimeout(function () {
+        downloadAllFiles(fileKeys);
+    }, 1000); // 1초 간격으로 다운로드 (이 시간은 필요에 따라 조절 가능)
+}
+async function downloadAllFilesInGrid() {
+    var zip = new JSZip();
+    var gridData = fileTreeGridView.target.getList();
+    var filesDownloaded = 0;
+
+    for (const item of gridData) {
+        if (item.fileKey) {
+            try {
+                const fileBlob = await getFileBlob(item.fileKey);
+                zip.file(item.fileName, fileBlob);
+                filesDownloaded++;
+            } catch (error) {
+                console.error("Error downloading file:", item.fileName, error);
+            }
+        }
+    }
+
+    if (filesDownloaded > 0) {
+        zip.generateAsync({ type: "blob" }).then(function (blob) {
+            var link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "files.zip";
+            link.click();
+        });
+    } else {
+        alert("No files to download");
+    }
+}
+
+function getFileBlob(fileKey) {
+    return new Promise(function (resolve, reject) {
+        postAjax("/admin/cm/cm08/fileDownInfo", { fileKey: fileKey }, null, function (data) {
+            var fileInfo = data.fileInfo;
+            var filePath = encodeURI(fileInfo.filePath + fileInfo.fileKey + "_" + fileInfo.fileName, "UTF-8");
+            fetch("/admin/cm/cm08/fileDownload?filePath=" + filePath)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return response.blob();
+                })
+                .then((blob) => {
+                    resolve(blob);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    });
+}
+
