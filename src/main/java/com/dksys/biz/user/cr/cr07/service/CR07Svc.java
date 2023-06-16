ebc924dbeb165e72d2d4d834a0641fe5945cdb63
jiwon <lavender_814@naver.com>
2023-06-16 18:10:13 +0900

    230616 매출확정등록

--- src/main/java/com/dksys/biz/user/cr/cr07/service/CR07Svc.java
+++ src/main/java/com/dksys/biz/user/cr/cr07/service/CR07Svc.java
@@ -5,20 +5,19 @@ import java.util.Map;
 
 public interface CR07Svc {
 	// 수주리스트
-	public int selectOrdrsDcsnCount(Map<String, String> paramMap);
-	public List<Map<String, String>> selectOrdrsDcsnList(Map<String, String> paramMap);
+	int selectOrdrsDcsnCount(Map<String, String> paramMap);
+	List<Map<String, String>> selectOrdrsDcsnList(Map<String, String> paramMap);
 
 	// 매출확정리스트 조회
-	public int selectSellDcsnCount(Map<String, String> paramMap);
-	public List<Map<String, String>> selectSellDcsnList(Map<String, String> paramMap);
+	int selectSellDcsnCount(Map<String, String> paramMap);
+	List<Map<String, String>> selectSellDcsnList(Map<String, String> paramMap);
 	
 	// 매출확정등록 조회
-	public int addSellDscnCount(Map<String, String> paramMap);
-	public List<Map<String, String>> addSellDscnList(Map<String, String> paramMap);
+	int addSellDscnCount(Map<String, String> paramMap);
+	List<Map<String, String>> addSellDscnList(Map<String, String> paramMap);
 	
 	// 매출확정등록 입력
-	public int insertSellDscn(Map<String, String> paramMap);
-	
+	int insertSellDscn(Map<String, String> paramMap);
 	
 
 }
