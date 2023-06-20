b0f99925e1d75c74c3f39a25d81fed3e503dc013
zsjc065 <zenji1106!>
2023-06-20 18:59:51 +0900

    제품마스터 팝업형태로 수정

--- src/main/java/com/dksys/biz/user/bm/bm10/service/BM10Svc.java
+++ src/main/java/com/dksys/biz/user/bm/bm10/service/BM10Svc.java
@@ -6,16 +6,21 @@ import java.util.Map;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface BM10Svc {
-	int selectBmPrdtMstrCount(Map<String, String> paramMap);
 
-	List<Map<String, String>> selectBmPrdtMstrList(Map<String, String> paramMap);
+  // int selectPchsCostCount(Map<String, String> paramMap);
+  int grid1_selectCount(Map<String, String> paramMap);
 
-	List<Map<String, String>> selectBmProdSearchList(Map<String, String> paramMap);
+  // List<Map<String, String>> selectPchsCostList(Map<String, String> paramMap);
+  List<Map<String, String>> grid1_selectList(Map<String, String> paramMap);
 
-	String insertBmPrdtMstr(Map<String, String> paramMap, MultipartHttpServletRequest mRequest);
+  Map<String, String> select_bm10_Info(Map<String, String> paramMap);
 
-	int updateBmPrdtMstr(Map<String, String> paramMap);
+  // int selectConfirmCount(Map<String, String> paramMap);
 
-	int selectBmProdCount(Map<String, String> paramMap);
+  int insert_bm10(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception;
+
+  int update_bm10(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception;
+
+  int delete_bm10(Map<String, String> paramMap) throws Exception;
 
 }
--- src/main/java/com/dksys/biz/user/bm/bm10/service/impl/BM10SvcImpl.java
+++ src/main/java/com/dksys/biz/user/bm/bm10/service/impl/BM10SvcImpl.java
@@ -1,66 +1,187 @@
 package com.dksys.biz.user.bm.bm10.service.impl;
 
+import java.lang.reflect.Type;
+import java.text.Format.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.dksys.biz.user.bm.bm10.mapper.BM10Mapper;
 import com.dksys.biz.user.bm.bm10.service.BM10Svc;
-import com.dksys.biz.util.ExceptionThrower;
+import com.dksys.biz.admin.cm.cm08.service.CM08Svc;
+import com.dksys.biz.admin.cm.cm15.service.CM15Svc;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class BM10SvcImpl implements BM10Svc {
-
 	@Autowired
 	BM10Mapper bm10Mapper;
-
+	
 	@Autowired
-	BM10Svc bm10Svc;
-
+	CM15Svc cm15Svc;
+	
 	@Autowired
-	ExceptionThrower thrower;
-
-//	@Override
-//	public int selectBmPrdtMstrCount(Map<String, String> paramMap) {
-//		return bm10Mapper.selectBmPrdtMstrCount(paramMap);
-//	}  
-
+	CM08Svc cm08Svc;
+	
 	@Override
-	public int selectBmPrdtMstrCount(Map<String, String> paramMap) {
-		// TODO Auto-generated method stub
-		return bm10Mapper.selectBmPrdtMstrCount(paramMap);
+	public int grid1_selectCount(Map<String, String> paramMap) {
+		return bm10Mapper.grid1_selectCount(paramMap);
 	}
-
+	
 	@Override
-	public int selectBmProdCount(Map<String, String> paramMap) {
-		// TODO Auto-generated method stub
-		return bm10Mapper.selectBmProdCount(paramMap);
+	public List<Map<String, String>> grid1_selectList(Map<String, String> paramMap) {
+		return bm10Mapper.grid1_selectList(paramMap);
 	}
-
+	
 	@Override
-	public List<Map<String, String>> selectBmPrdtMstrList(Map<String, String> paramMap) {
-		return bm10Mapper.selectBmPrdtMstrList(paramMap);
+	public Map<String, String> select_bm10_Info(Map<String, String> paramMap) {
+		return bm10Mapper.select_bm10_Info(paramMap);
 	}
-
+	
+	// @Override
+	// public int selectConfirmCount(Map<String, String> paramMap) {
+	// 	return bm10Mapper.selectConfirmCount(paramMap);
+	// }
+	
 	@Override
-	public List<Map<String, String>> selectBmProdSearchList(Map<String, String> paramMap) {
-		return bm10Mapper.selectBmProdSearchList(paramMap);
+	public int update_bm10(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+		//Gson gson = new Gson();
+		Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+		Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
+		
+		//---------------------------------------------------------------
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+		//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------
+		HashMap<String, String> param = new HashMap<>();
+		param.put("userId", paramMap.get("userId"));
+		param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
+		
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+			//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
+			param.put("jobType", "fileUp");
+			cm15Svc.selectFileAuthCheck(param);
+		}
+		
+		String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
+		List<String> deleteFileList = Arrays.asList(deleteFileArr);
+		
+		for(String fileKey : deleteFileList) {
+			// 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
+			Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
+			//접근 권한 없으면 Exception 발생
+			param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
+			param.put("jobType", "fileDelete");
+			cm15Svc.selectFileAuthCheck(param);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크  끝
+		//---------------------------------------------------------------
+		
+		int result = bm10Mapper.update_bm10(paramMap);
+		
+		//---------------------------------------------------------------
+		//첨부 화일 처리 시작
+		//---------------------------------------------------------------
+		if (uploadFileList.size() > 0) {
+			paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+			paramMap.put("fileTrgtKey", paramMap.get("fileTrgtKey"));
+			cm08Svc.uploadFile(paramMap, mRequest);
+		}
+		
+		for(String fileKey : deleteFileList) {
+			cm08Svc.deleteFile(fileKey);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 처리  끝
+		//---------------------------------------------------------------
+		return result;
 	}
-
+	
 	@Override
-	public String insertBmPrdtMstr(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) {
-		return bm10Mapper.insertBmPrdtMstr(paramMap, mRequest);
+	public int insert_bm10(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+		Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+		Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+		
+		//---------------------------------------------------------------
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+		//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+			//접근 권한 없으면 Exception 발생
+			paramMap.put("jobType", "fileUp");
+			cm15Svc.selectFileAuthCheck(paramMap);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크  끝
+		//---------------------------------------------------------------
+		
+		int fileTrgtKey = bm10Mapper.select_bm10_SeqNext(paramMap);
+		paramMap.put("fileTrgtKey", Integer.toString(fileTrgtKey));
+		
+		int result = bm10Mapper.insert_bm10(paramMap);
+		//---------------------------------------------------------------
+		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
+		//---------------------------------------------------------------
+		if (uploadFileList.size() > 0) {
+			paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+			paramMap.put("fileTrgtKey", paramMap.get("fileTrgtKey"));
+			cm08Svc.uploadFile(paramMap, mRequest);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 처리  끝
+		//---------------------------------------------------------------
+		return result;
 	}
-
+	
 	@Override
-	public int updateBmPrdtMstr(Map<String, String> paramMap) {
-		return bm10Mapper.updateBmPrdtMstr(paramMap);
-
+	public int delete_bm10(Map<String, String> paramMap) throws Exception {
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크  시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
+		//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------
+		List<Map<String, String>> deleteFileList = cm08Svc.selectFileListAll(paramMap);
+		HashMap<String, String> param = new HashMap<>();
+		param.put("jobType", "fileDelete");
+		param.put("userId", paramMap.get("userId"));
+		if (deleteFileList.size() > 0) {
+			for (Map<String, String> dtl : deleteFileList) {
+				//접근 권한 없으면 Exception 발생
+				param.put("comonCd",  dtl.get("comonCd"));
+				cm15Svc.selectFileAuthCheck(param);
+			}
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크 끝
+		//---------------------------------------------------------------
+		
+		int result = bm10Mapper.delete_bm10(paramMap);
+		//---------------------------------------------------------------
+		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
+		//---------------------------------------------------------------
+		if (deleteFileList.size() > 0) {
+			for (Map<String, String> deleteDtl : deleteFileList) {
+				String fileKey = deleteDtl.get("fileKey").toString();
+				cm08Svc.deleteFile( fileKey );
+			}
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 처리  끝
+		//---------------------------------------------------------------
+		return result;
 	}
 
 }
