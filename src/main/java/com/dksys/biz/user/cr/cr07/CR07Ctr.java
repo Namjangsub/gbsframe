6c5fd5191a36a7a583677de065ae7e23dbaeebc5
jiwon <lavender_814@naver.com>
2023-06-23 09:03:48 +0900

    매출확정관리 -1

--- src/main/java/com/dksys/biz/user/cr/cr07/CR07Ctr.java
+++ src/main/java/com/dksys/biz/user/cr/cr07/CR07Ctr.java
@@ -22,11 +22,10 @@ import com.dksys.biz.util.MessageUtils;
 import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
 @Controller
-@Transactional(rollbackFor = Exception.class)
 @RequestMapping("/user/cr/cr07")
 public class CR07Ctr {
 	
-	private Logger logger = LoggerFactory.getLogger(CR07Ctr.class);
+//	private Logger logger = LoggerFactory.getLogger(CR07Ctr.class);
 	
 	@Autowired
 	MessageUtils messageUtils;
