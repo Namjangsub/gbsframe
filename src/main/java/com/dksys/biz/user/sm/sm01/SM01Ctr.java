8e5448f1aff414eb60907316968819088b55767e
baetaeyong <->
2023-06-26 20:06:29 +0900

    구매Bom 수정

--- src/main/java/com/dksys/biz/user/sm/sm01/SM01Ctr.java
+++ src/main/java/com/dksys/biz/user/sm/sm01/SM01Ctr.java
@@ -1,80 +1,171 @@
 package com.dksys.biz.user.sm.sm01;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import com.dksys.biz.cmn.vo.PaginationInfo;
 import com.dksys.biz.user.sm.sm01.service.SM01Svc;
+import com.dksys.biz.cmn.vo.PaginationInfo;
 import com.dksys.biz.util.MessageUtils;
 
 @Controller
 @RequestMapping("/user/sm/sm01")
 public class SM01Ctr {
-	
-	@Autowired
-	MessageUtils messageUtils;
-	
-	@Autowired
-	SM01Svc sm01Svc;
-	
-	// 구매BOM관리 Master 조회
+
+  @Autowired
+  MessageUtils messageUtils;
+
+  @Autowired
+  SM01Svc sm01Svc;
+
+    //구매BOM관리 Master 조회
 	@PostMapping(value = "/selectBomSalesList")
 	public String selectBomSalesList(@RequestBody Map<String, String> paramMap, ModelMap model) {
 		int totalCnt = sm01Svc.selectBomSalesCount(paramMap);
 		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
 		model.addAttribute("paginationInfo", paginationInfo);
-		
-		List<Map<String, String>> resultList = sm01Svc.selectBomSalesList(paramMap);
-		model.addAttribute("resultList", resultList);
+		List<Map<String, String>> result = sm01Svc.selectBomSalesList(paramMap);
+		model.addAttribute("resultList", result);
 		return "jsonView";
 	}
-	
+
 	// BOM내역상세 조회
 	@PostMapping(value = "/selectBomDetailList")
 	public String selectBomDetailList(@RequestBody Map<String, String> paramMap, ModelMap model) {
 		int totalCnt = sm01Svc.selectBomDetailCount(paramMap);
 		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
 		model.addAttribute("paginationInfo", paginationInfo);
-		
 		List<Map<String, String>> resultList = sm01Svc.selectBomDetailList(paramMap);
-		model.addAttribute("resultList", resultList);
-		return "jsonView";
-		
-	}
-	
-	// 매출확정등록 조회
-	@PostMapping(value = "/addSellDscnList")
-	public String addSellDscn(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = sm01Svc.addSellDscnCount(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
-		
-		List<Map<String, String>> resultList = sm01Svc.addSellDscnList(paramMap);
 		model.addAttribute("resultList", resultList);
 		return "jsonView";
 	}
 
-	// 매출확정등록 입력
-	@PostMapping(value = "/insertSellDscn")
-	public String insertSellDscn(@RequestParam Map<String, String> paramMap, MultipartHttpServletRequest mRequest, ModelMap model) {
-		try {
-			sm01Svc.insertSellDscn(paramMap);
-			model.addAttribute("resultCode", 200);
-			model.addAttribute("resultMessage", messageUtils.getMessage("insert"));
-		} catch (Exception e) {
-			model.addAttribute("resultCode", 500);
-			model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
-		}
+	//Bom 리스트
+	@PostMapping("/selectBuyBomList")
+	public String selectBuyBomList(@RequestBody Map<String, String> paramMap, ModelMap model) {
+		List<Map<String, String>> resultList = sm01Svc.selectBuyBomList(paramMap);
+		model.addAttribute("resultList", resultList);
 		return "jsonView";
 	}
-	
+
+	// 자재 리스트 조회
+	@PostMapping("/selectBomMatrList")
+	public String selectBomMatrList(@RequestBody Map<String, String> paramMap, ModelMap model) {
+		List<Map<String, String>> prdtList = sm01Svc.selectBomMatrList(paramMap);
+		model.addAttribute("resultList", prdtList);
+		return "jsonView";
+	}
+  // 프로젝트 정보 조회
+  @PostMapping(value = "/selectPrjctInfo")
+  public String selectPrjctInfo(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    Map<String, String> result = sm01Svc.selectPrjctInfo(paramMap);
+    model.addAttribute("result", result);
+    return "jsonView";
+  }
+  
+  @PostMapping(value = "/insertBom")
+  public String insertBom(@RequestParam Map<String, String> paramMap, MultipartHttpServletRequest mRequest, ModelMap model) throws Exception {
+		try {
+			if (sm01Svc.insertBom(paramMap, mRequest) != 0 ) {
+				model.addAttribute("resultCode", 200);
+				model.addAttribute("resultMessage", messageUtils.getMessage("insert"));
+			} else {
+				model.addAttribute("resultCode", 500);
+				model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+			};
+		}catch(Exception e){
+			model.addAttribute("resultCode", 900);
+			model.addAttribute("resultMessage", e.getMessage());
+		}
+		return "jsonView";
+  }
+
+  @PostMapping(value = "/updateBom")
+  public String updateBom(@RequestParam Map<String, String> paramMap, MultipartHttpServletRequest mRequest, ModelMap model) throws Exception {
+	  	try {
+			if (sm01Svc.updateBom(paramMap, mRequest) != 0 ) {
+				model.addAttribute("resultCode", 200);
+				model.addAttribute("resultMessage", messageUtils.getMessage("update"));
+			} else {
+				model.addAttribute("resultCode", 500);
+				model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+			};
+		}catch(Exception e){
+			model.addAttribute("resultCode", 900);
+			model.addAttribute("resultMessage", e.getMessage());
+		}
+	  	return "jsonView";
+  }
+
+  @PutMapping(value = "/deleteBom")
+  public String deleteBom(@RequestBody Map<String, String> paramMap, ModelMap model) throws Exception {
+	  	try {
+			if (sm01Svc.deleteBom(paramMap) != 0 ) {
+				model.addAttribute("resultCode", 200);
+				model.addAttribute("resultMessage", messageUtils.getMessage("delete"));
+			} else {
+				model.addAttribute("resultCode", 500);
+				model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+			};
+		}catch(Exception e){
+			model.addAttribute("resultCode", 900);
+			model.addAttribute("resultMessage", e.getMessage());
+		}
+	  	return "jsonView";
+  }
+
+
+  // 프로젝트 이슈정보 조회
+  @PostMapping(value = "/selectPrjctIssueInfo")
+  public String selectPrjctIssueInfo(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    Map<String, String> result = sm01Svc.selectPrjctIssueInfo(paramMap);
+    model.addAttribute("result", result);
+    return "jsonView";
+  }
+    
+  @PostMapping(value = "/insertBomIssue")
+  public String insertBomIssue(@RequestParam Map<String, String> paramMap, MultipartHttpServletRequest mRequest, ModelMap model) throws Exception {
+	  try {
+		  if (sm01Svc.insertBomIssue(paramMap, mRequest) != 0 ) {
+			  model.addAttribute("resultCode", 200);
+			  model.addAttribute("resultMessage", messageUtils.getMessage("insert"));
+		  } else {
+			  model.addAttribute("resultCode", 500);
+			  model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+		  };
+	  }catch(Exception e){
+		  model.addAttribute("resultCode", 900);
+		  model.addAttribute("resultMessage", e.getMessage());
+	  }
+	  return "jsonView";
+  }
+  
+  @PostMapping(value = "/updateBomIssue")
+  public String updateBomIssue(@RequestParam Map<String, String> paramMap, MultipartHttpServletRequest mRequest, ModelMap model) throws Exception {
+	  try {
+		  if (sm01Svc.updateBomIssue(paramMap, mRequest) != 0 ) {
+			  model.addAttribute("resultCode", 200);
+			  model.addAttribute("resultMessage", messageUtils.getMessage("update"));
+		  } else {
+			  model.addAttribute("resultCode", 500);
+			  model.addAttribute("resultMessage", messageUtils.getMessage("fail"));
+		  };
+	  }catch(Exception e){
+		  model.addAttribute("resultCode", 900);
+		  model.addAttribute("resultMessage", e.getMessage());
+	  }
+	  return "jsonView";
+  }
+  
 }
--- src/main/java/com/dksys/biz/user/sm/sm01/service/impl/SM01SvcImpl.java
+++ src/main/java/com/dksys/biz/user/sm/sm01/service/impl/SM01SvcImpl.java
@@ -1,64 +1,286 @@
 package com.dksys.biz.user.sm.sm01.service.impl;
 
+import java.lang.reflect.Type;
+import java.text.Format.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.dksys.biz.user.sm.sm01.mapper.SM01Mapper;
 import com.dksys.biz.user.sm.sm01.service.SM01Svc;
-import com.dksys.biz.util.ExceptionThrower;
+import com.dksys.biz.admin.cm.cm08.service.CM08Svc;
+import com.dksys.biz.admin.cm.cm15.service.CM15Svc;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class SM01SvcImpl implements SM01Svc {
+
+  @Autowired
+  SM01Mapper sm01Mapper;
+
+  @Autowired
+  CM15Svc cm15Svc;
+  
+  @Autowired
+  CM08Svc cm08Svc;
+  
+  @Override
+  public int selectBomSalesCount(Map<String, String> paramMap) {
+    return sm01Mapper.selectBomSalesCount(paramMap);
+  }
+
+  @Override
+  public List<Map<String, String>> selectBomSalesList(Map<String, String> paramMap) {
+    return sm01Mapper.selectBomSalesList(paramMap);
+  }
 	
-	@Autowired
-	SM01Mapper sm01Mapper;
+  @Override
+  public int selectBomDetailCount(Map<String, String> paramMap) {
+	return sm01Mapper.selectBomDetailCount(paramMap);
+  }
 	
-	@Autowired
-	SM01Svc sm01Svc;
+  @Override
+  public List<Map<String, String>> selectBomDetailList(Map<String, String> paramMap) {
+    return sm01Mapper.selectBomDetailList(paramMap);
+  }
+
+  @Override
+  public List<Map<String, String>> selectBuyBomList(Map<String, String> paramMap) {
+    return sm01Mapper.selectBuyBomList(paramMap);
+  }
+
+  @Override
+  public List<Map<String, String>> selectBomMatrList(Map<String, String> paramMap) {
+    return sm01Mapper.selectBomMatrList(paramMap);
+  }
+
+  @Override
+  public Map<String, String> selectPrjctInfo(Map<String, String> paramMap) {
+    return sm01Mapper.selectPrjctInfo(paramMap);
+  }
+
+  @Override
+  public int insertBom(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+
+	    Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+	    Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+	    
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+	  	//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------  
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+				//접근 권한 없으면 Exception 발생
+				paramMap.put("jobType", "fileUp");
+				cm15Svc.selectFileAuthCheck(paramMap);
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 권한체크  끝 
+		//---------------------------------------------------------------  
+		//String  newPrjctSeq = String.valueOf(sm01Mapper.selectPrjctSeqNext(paramMap));
+		//paramMap.put("prjctSeq", newPrjctSeq);
+		int result = sm01Mapper.insertBom(paramMap);
 	
-	@Autowired
-	ExceptionThrower thrower;
-	
-	@Override
-	public int selectBomDetailCount(Map<String, String> paramMap) {
-		return sm01Mapper.selectBomDetailCount(paramMap);
+	    List<Map<String, String>> bomList = gsonDtl.fromJson(paramMap.get("bomArr"), dtlMap);
+	    for (Map<String, String> dtl : bomList) {
+	    	//dtl.put("prjctSeq", newPrjctSeq);
+	    	dtl.put("userId", paramMap.get("userId"));
+	    	dtl.put("pgmId", paramMap.get("pgmId"));
+	    }
+	    List<Map<String, String>> matrList = gsonDtl.fromJson(paramMap.get("matrArr"), dtlMap);
+	    for (Map<String, String> dtl : matrList) {
+	    	//dtl.put("prjctSeq", newPrjctSeq);
+	    	dtl.put("userId", paramMap.get("userId"));
+	    	dtl.put("pgmId", paramMap.get("pgmId"));
+	    	//반복문에서는 각 맵(dtl)에 "userId"와 "pgmId"를 추가
+	    	String dtaChk = dtl.get("dtaChk").toString();
+	    	/* "dtaChk" 값을 확인하여
+	    	 * "I"인 경우 sm01Mapper.insertBomMatr(dtl)을 호출하여 프로젝트 세부정보를 삽입 */
+	    	if ("I".equals(dtaChk)) {
+	    		sm01Mapper.insertBomMatr(dtl);
+	    	} 
+	    }
+	    
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
+		//---------------------------------------------------------------  
+		if (uploadFileList.size() > 0) {
+		    paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+		    paramMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+		    cm08Svc.uploadFile(paramMap, mRequest);
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 처리  끝 
+		//---------------------------------------------------------------  
+	    
+	    return result;
+  }
+  @Override
+  public int updateBom(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+//	Gson gson = new Gson();
+	Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+	Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
+
+	//---------------------------------------------------------------  
+	//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+  	//   필수값 :  jobType, userId, comonCd
+	//---------------------------------------------------------------  
+    HashMap<String, String> param = new HashMap<>();
+    param.put("userId", paramMap.get("userId"));
+    param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
+    
+	List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+	if (uploadFileList.size() > 0) {
+			//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
+			param.put("jobType", "fileUp");
+			cm15Svc.selectFileAuthCheck(param);
 	}
-	
-	@Override
-	public List<Map<String, String>> selectBomDetailList(Map<String, String> paramMap) {
-		return sm01Mapper.selectBomDetailList(paramMap);
+	String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
+	List<String> deleteFileList = Arrays.asList(deleteFileArr);
+    for(String fileKey : deleteFileList) {  // 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
+		    Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
+			//접근 권한 없으면 Exception 발생
+		    param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
+		    param.put("jobType", "fileDelete");
+			cm15Svc.selectFileAuthCheck(param);
 	}
-	
-	@Override
-	public int selectBomSalesCount(Map<String, String> paramMap) {
-		return sm01Mapper.selectBomSalesCount(paramMap);
-	}
-	
-	@Override
-	public List<Map<String, String>> selectBomSalesList(Map<String, String> paramMap) {
-		return sm01Mapper.selectBomSalesList(paramMap);
-	}
-	
-	@Override
-	public int addSellDscnCount(Map<String, String> paramMap) {
-		return sm01Mapper.addSellDscnCount(paramMap);
-	}
-	
-	@Override
-	public List<Map<String, String>> addSellDscnList(Map<String, String> paramMap) {
-		return sm01Mapper.addSellDscnList(paramMap);
-	}
-	
-	@Override
-	public int insertSellDscn(Map<String, String> paramMap) {
-		int result = sm01Mapper.insertSellDscn(paramMap);
-		return result;
-		
-	}
-	
+	//---------------------------------------------------------------  
+	//첨부 화일 권한체크  끝 
+	//---------------------------------------------------------------  
+
+	int result = sm01Mapper.updateBom(paramMap);
+    	//  sm01Mapper.updateBom(paramMap)을 호출하여 paramMap을 사용하여 프로젝트를 업데이트하고 그 결과를 result 변수에 저장.
+
+    List<Map<String, String>> bomList = gsonDtl.fromJson(paramMap.get("bomArr"), dtlMap);
+    for (Map<String, String> dtl : bomList) {
+    	//  matrList 리스트의 각 맵 요소에 대해 반복문을 실행
+    	
+    	dtl.put("userId", paramMap.get("userId"));
+    	dtl.put("pgmId", paramMap.get("pgmId"));
+    	//      반복문에서는 각 맵(dtl)에 "userId"와 "pgmId"를 추가
+    	String dtaChk = dtl.get("prjctCrudChk").toString();
+    	/* "dtaChk" 값을 확인하여
+    	 * "I"인 경우 sm01Mapper.insertBomMatr(dtl)을 호출하여 프로젝트 세부정보를 삽입하고,
+    	 * "U"인 경우 sm01Mapper.updateBomMatr(dtl)을 호출하여 프로젝트 세부정보를 업데이트하고,
+    	 * "D"인 경우 * sm01Mapper.deleteBomMatr(dtl)을 호출하여 프로젝트 세부정보를 삭제.		 */
+    	if ("I".equals(dtaChk)) {
+    		dtl.put("prjctSeq", paramMap.get("prjctSeq"));
+    		sm01Mapper.insertBom(dtl);
+    	} else if ("U".equals(dtaChk)) {
+    		sm01Mapper.updateBom(dtl);
+    	} else if ("D".equals(dtaChk)) {
+    		sm01Mapper.deleteBomMatrAll(dtl);
+    		sm01Mapper.deleteBom(dtl);
+    	}
+    }
+    
+    List<Map<String, String>> matrList = gsonDtl.fromJson(paramMap.get("matrArr"), dtlMap);
+    for (Map<String, String> dtl : matrList) {
+    	//  matrList 리스트의 각 맵 요소에 대해 반복문을 실행
+
+      dtl.put("userId", paramMap.get("userId"));
+      dtl.put("pgmId", paramMap.get("pgmId"));
+      //      반복문에서는 각 맵(dtl)에 "userId"와 "pgmId"를 추가
+      String dtaChk = dtl.get("dtaChk").toString();
+		/* "dtaChk" 값을 확인하여
+		 * "I"인 경우 sm01Mapper.insertBomMatr(dtl)을 호출하여 프로젝트 세부정보를 삽입하고,
+		 * "U"인 경우 sm01Mapper.updateBomMatr(dtl)을 호출하여 프로젝트 세부정보를 업데이트하고,
+		 * "D"인 경우 * sm01Mapper.deleteBomMatr(dtl)을 호출하여 프로젝트 세부정보를 삭제.		 */
+      if ("I".equals(dtaChk)) {
+	        sm01Mapper.insertBomMatr(dtl);
+	      } else if ("U".equals(dtaChk)) {
+	        sm01Mapper.updateBomMatr(dtl);
+	      } else if ("D".equals(dtaChk)) {
+	        sm01Mapper.deleteBomMatr(dtl);
+	      }
+    }
+    
+	//---------------------------------------------------------------  
+	//첨부 화일 처리 시작 
+	//---------------------------------------------------------------  
+    if (uploadFileList.size() > 0) {
+	    paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+	    paramMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+	    cm08Svc.uploadFile(paramMap, mRequest);
+    }
+    
+    for(String fileKey : deleteFileList) {
+    	cm08Svc.deleteFile(fileKey);
+    }
+	//---------------------------------------------------------------  
+	//첨부 화일 처리  끝 
+	//---------------------------------------------------------------  
+    
+     return result;
+  }
+
+  @Override
+  public int deleteBom(Map<String, String> paramMap) throws Exception {
+	    //---------------------------------------------------------------  
+		//첨부 화일 권한체크  시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
+	  	//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------  
+	    List<Map<String, String>> deleteFileList = cm08Svc.selectFileListAll(paramMap);
+	    HashMap<String, String> param = new HashMap<>();
+	    param.put("jobType", "fileDelete");
+	    param.put("userId", paramMap.get("userId"));
+	    if (deleteFileList.size() > 0) {
+		    for (Map<String, String> dtl : deleteFileList) {
+					//접근 권한 없으면 Exception 발생
+		            param.put("comonCd",  dtl.get("comonCd"));
+			    	
+					cm15Svc.selectFileAuthCheck(param);
+			}
+	    }
+		//---------------------------------------------------------------  
+		//첨부 화일 권한체크 끝 
+		//---------------------------------------------------------------  
+	  
+	  int result = sm01Mapper.deleteBomMatrAll(paramMap);
+		  result = sm01Mapper.deleteBom(paramMap);
+	  
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
+		//---------------------------------------------------------------  
+		if (deleteFileList.size() > 0) {		  
+		    for (Map<String, String> deleteDtl : deleteFileList) {
+		    	String fileKey = deleteDtl.get("fileKey").toString();
+			    cm08Svc.deleteFile( fileKey );
+		    }
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 처리  끝 
+		//---------------------------------------------------------------  	  
+	    return result;
+  }
+
+  @Override
+  public Map<String, String> selectPrjctIssueInfo(Map<String, String> paramMap) {
+	  return sm01Mapper.selectPrjctIssueInfo(paramMap);
+  }
+ 
+  @Override
+  public int insertBomIssue(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+	  int result = sm01Mapper.insertBomIssue(paramMap);
+	  return result;
+  }
+  
+  @Override
+  public int updateBomIssue(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+	  int result = sm01Mapper.updateBomIssue(paramMap);
+	  return result;
+  }
+  
 }
