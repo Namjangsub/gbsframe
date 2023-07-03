4c09204c0913d9e1f79865f0a8fb0a66ba4208b7
jiwon <lavender_814@naver.com>
2023-07-03 16:44:08 +0900

    매출확정관리 update 수정

--- src/main/java/com/dksys/biz/user/cr/cr07/service/impl/CR07SvcImpl.java
+++ src/main/java/com/dksys/biz/user/cr/cr07/service/impl/CR07SvcImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dksys.biz.admin.cm.cm08.service.CM08Svc;
 import com.dksys.biz.admin.cm.cm15.service.CM15Svc;
@@ -37,8 +38,6 @@ public class CR07SvcImpl implements CR07Svc {
 	@Autowired
 	CM08Svc cm08Svc;
 	
-	@Autowired
-	ExceptionThrower thrower;
 	@Override
 	public int selectOrdrsDcsnCount(Map<String, String> paramMap) {
 		return cr07Mapper.selectOrdrsDcsnCount(paramMap);
@@ -129,94 +128,94 @@ public class CR07SvcImpl implements CR07Svc {
 	@Override
 	public int updateSellDscn(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
 		//Gson gson = new Gson();
-				Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
-				Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
-				
-				//---------------------------------------------------------------
-				//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
-				//   필수값 :  jobType, userId, comonCd
-				//---------------------------------------------------------------
-				HashMap<String, String> param = new HashMap<>();
-				param.put("userId", paramMap.get("userId"));
-				param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
-				
-				List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
-				if (uploadFileList.size() > 0) {
-					//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
-					param.put("jobType", "fileUp");
-					cm15Svc.selectFileAuthCheck(param);
-				}
-				
-				String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
-				List<String> deleteFileList = Arrays.asList(deleteFileArr);
-				
-				for(String fileKey : deleteFileList) {
-					// 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
-					Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
-					//접근 권한 없으면 Exception 발생
-					param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
-					param.put("jobType", "fileDelete");
-					cm15Svc.selectFileAuthCheck(param);
-				}
-				//---------------------------------------------------------------
-				//첨부 화일 권한체크  끝
-				//---------------------------------------------------------------
-				
-				int result = cr07Mapper.updateSellDscn(paramMap);
-				
-				//---------------------------------------------------------------
-				//첨부 화일 처리 시작
-				//---------------------------------------------------------------
-				if (uploadFileList.size() > 0) {
-					paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
-					paramMap.put("fileTrgtKey", paramMap.get("fileTrgtKey"));
-					cm08Svc.uploadFile(paramMap, mRequest);
-				}
-				
-				for(String fileKey : deleteFileList) {
-					cm08Svc.deleteFile(fileKey);
-				}
-				//---------------------------------------------------------------
-				//첨부 화일 처리  끝
-				//---------------------------------------------------------------
-				
-				return result;
+		Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+		Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
+		
+		//---------------------------------------------------------------
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+		//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------
+		HashMap<String, String> param = new HashMap<>();
+		param.put("userId", paramMap.get("userId"));
+		param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
+		
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+			//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
+			param.put("jobType", "fileUp");
+			cm15Svc.selectFileAuthCheck(param);
+		}
+		
+		String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
+		List<String> deleteFileList = Arrays.asList(deleteFileArr);
+		
+		for(String fileKey : deleteFileList) {
+			// 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
+			Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
+			//접근 권한 없으면 Exception 발생
+			param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
+			param.put("jobType", "fileDelete");
+			cm15Svc.selectFileAuthCheck(param);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크  끝
+		//---------------------------------------------------------------
+		
+		int result = cr07Mapper.updateSellDscn(paramMap);
+		
+		//---------------------------------------------------------------
+		//첨부 화일 처리 시작
+		//---------------------------------------------------------------
+		if (uploadFileList.size() > 0) {
+			paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+			paramMap.put("fileTrgtKey", paramMap.get("fileTrgtKey"));
+			cm08Svc.uploadFile(paramMap, mRequest);
+		}
+		
+		for(String fileKey : deleteFileList) {
+			cm08Svc.deleteFile(fileKey);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 처리  끝
+		//---------------------------------------------------------------
+		
+		return result;
 	}
 	
 	@Override
 	public int delete_cr07(Map<String, String> paramMap) throws Exception {
 		//---------------------------------------------------------------
-				//첨부 화일 권한체크  시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
-				//   필수값 :  jobType, userId, comonCd
-				//---------------------------------------------------------------
-				List<Map<String, String>> deleteFileList = cm08Svc.selectFileListAll(paramMap);
-				HashMap<String, String> param = new HashMap<>();
-				param.put("jobType", "fileDelete");
-				param.put("userId", paramMap.get("userId"));
-				if (deleteFileList.size() > 0) {
-					for (Map<String, String> dtl : deleteFileList) {
-						//접근 권한 없으면 Exception 발생
-						param.put("comonCd",  dtl.get("comonCd"));
-						cm15Svc.selectFileAuthCheck(param);
-					}
-				}
-				//---------------------------------------------------------------
-				//첨부 화일 권한체크 끝
-				//---------------------------------------------------------------
-				
-				int result = cr07Mapper.delete_cr07(paramMap);
-				//---------------------------------------------------------------
-				//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
-				//---------------------------------------------------------------
-				if (deleteFileList.size() > 0) {
-					for (Map<String, String> deleteDtl : deleteFileList) {
-						String fileKey = deleteDtl.get("fileKey").toString();
-						cm08Svc.deleteFile( fileKey );
-					}
-				}
-				//---------------------------------------------------------------
-				//첨부 화일 처리  끝
-				//---------------------------------------------------------------
+		//첨부 화일 권한체크  시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
+		//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------
+		List<Map<String, String>> deleteFileList = cm08Svc.selectFileListAll(paramMap);
+		HashMap<String, String> param = new HashMap<>();
+		param.put("jobType", "fileDelete");
+		param.put("userId", paramMap.get("userId"));
+		if (deleteFileList.size() > 0) {
+			for (Map<String, String> dtl : deleteFileList) {
+				//접근 권한 없으면 Exception 발생
+				param.put("comonCd",  dtl.get("comonCd"));
+				cm15Svc.selectFileAuthCheck(param);
+			}
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크 끝
+		//---------------------------------------------------------------
+		
+		int result = cr07Mapper.delete_cr07(paramMap);
+		//---------------------------------------------------------------
+		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
+		//---------------------------------------------------------------
+		if (deleteFileList.size() > 0) {
+			for (Map<String, String> deleteDtl : deleteFileList) {
+				String fileKey = deleteDtl.get("fileKey").toString();
+				cm08Svc.deleteFile( fileKey );
+			}
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 처리  끝
+		//---------------------------------------------------------------
 		
 		return  result;
 	}
