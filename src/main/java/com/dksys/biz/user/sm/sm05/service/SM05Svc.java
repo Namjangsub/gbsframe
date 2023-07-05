87326b9fef9e9780453c9737132dbe870df1a2f3
HWKim <hwkim@sortech.co.kr>
2023-07-05 16:51:44 +0900

    0705 update 첨부파일, delete 진행

--- src/main/java/com/dksys/biz/user/sm/sm05/service/SM05Svc.java
+++ src/main/java/com/dksys/biz/user/sm/sm05/service/SM05Svc.java
@@ -7,31 +7,36 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface SM05Svc {
 
-	int selectIoCount(Map<String, String> paramMap);
-	List<Map<String, String>> selectIoList(Map<String, String> paramMap);
+	int select_sm05_ioCount(Map<String, String> paramMap);
+	List<Map<String, String>> select_sm05_ioList(Map<String, String> paramMap);
 
-	int selectIoDetailCount(Map<String, String> paramMap);
-	List<Map<String, String>> selectIoDetailList(Map<String, String> paramMap);
+	int select_sm05_ioDetailCount(Map<String, String> paramMap);
+	List<Map<String, String>> select_sm05_ioDetailList(Map<String, String> paramMap);
 
 //	int selectIoOutWhCount(Map<String, String> paramMap);
 //	List<Map<String, String>> selectIoOutWhList(Map<String, String> paramMap);
 
-	Map<String, String> select_sm05_IoInfo(Map<String, String> paramMap);
+	Map<String, String> select_sm05_ioInfo(Map<String, String> paramMap);
 	
 	public List<Map<String, Object>> selectOutWhNm(Map<String, String> param);
 	
 	// 폐기창고재고정보 - 모달창 
-	int selectIoModalCount(Map<String, String> paramMap);
-	List<Map<String, String>> selectIoModalList(Map<String, String> paramMap);
+	int select_sm05_ioModalCount(Map<String, String> paramMap);
+	List<Map<String, String>> select_sm05_ioModalList(Map<String, String> paramMap);
 	
 	//폐기창고 재고정보 insert
 	int insert_sm05(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception;
 	//폐기창고 insert
 	int insert_sm05_IoInfo(Map<String, String> paramMap, MultipartHttpServletRequest mRequest);
 
-	//수정
+	//수정 
 	int update_sm05(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception;
 	
+	int update_sm05_IoInfo(Map<String, String> paramMap, MultipartHttpServletRequest mRequest);
+	
+	//삭제
+	int delete_sm05M_io(Map<String, String> paramMap) throws Exception;
+	int delete_sm05D_io(Map<String, String> paramMap);
 	//Map<String, String> selectIoInfo(Map<String, String> paramMap);
 
 //	int selectPchsDetailCount(Map<String, String> paramMap);
@@ -44,6 +49,6 @@ public interface SM05Svc {
 //
 //	int updatePchsCost(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception;
 //
-//	int deleteIoCost(Map<String, String> paramMap) throws Exception;
+	
 
 }
--- src/main/java/com/dksys/biz/user/sm/sm05/service/impl/SM05SvcImpl.java
+++ src/main/java/com/dksys/biz/user/sm/sm05/service/impl/SM05SvcImpl.java
@@ -35,23 +35,23 @@ public class SM05SvcImpl implements SM05Svc {
 	CM08Svc cm08Svc;
 
 	@Override
-	public int selectIoCount(Map<String, String> paramMap) {
-		return sm05Mapper.selectIoCount(paramMap);
+	public int select_sm05_ioCount(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioCount(paramMap);
 	}
 
 	@Override
-	public List<Map<String, String>> selectIoList(Map<String, String> paramMap) {
-		return sm05Mapper.selectIoList(paramMap);
+	public List<Map<String, String>> select_sm05_ioList(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioList(paramMap);
 	}
 
 	@Override
-	public int selectIoDetailCount(Map<String, String> paramMap) {
-		return sm05Mapper.selectIoDetailCount(paramMap);
+	public int select_sm05_ioDetailCount(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioDetailCount(paramMap);
 	}
 
 	@Override
-	public List<Map<String, String>> selectIoDetailList(Map<String, String> paramMap) {
-		return sm05Mapper.selectIoDetailList(paramMap);
+	public List<Map<String, String>> select_sm05_ioDetailList(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioDetailList(paramMap);
 	}
 	
 //	@Override
@@ -65,8 +65,8 @@ public class SM05SvcImpl implements SM05Svc {
 
 	// 수정화면 정보
 	@Override
-	public Map<String, String> select_sm05_IoInfo(Map<String, String> paramMap) {
-		return sm05Mapper.select_sm05_IoInfo(paramMap);
+	public Map<String, String> select_sm05_ioInfo(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioInfo(paramMap);
 	}
 	
 	/* @Override */
@@ -76,12 +76,12 @@ public class SM05SvcImpl implements SM05Svc {
     }
     
     @Override
-	public int selectIoModalCount(Map<String, String> paramMap) {
-		return sm05Mapper.selectIoModalCount(paramMap);
+	public int select_sm05_ioModalCount(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioModalCount(paramMap);
 	}	
 	@Override
-	public List<Map<String, String>> selectIoModalList(Map<String, String> paramMap) {
-		return sm05Mapper.selectIoModalList(paramMap);
+	public List<Map<String, String>> select_sm05_ioModalList(Map<String, String> paramMap) {
+		return sm05Mapper.select_sm05_ioModalList(paramMap);
 	}
 
 //	@Override
@@ -145,7 +145,7 @@ public class SM05SvcImpl implements SM05Svc {
 	@Override
 	public int insert_sm05_IoInfo(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) {
 		int result = sm05Mapper.insert_sm05_IoInfo(paramMap);
-		
+		System.out.println(result);
 		return result;
 	}
 	
@@ -205,43 +205,56 @@ public class SM05SvcImpl implements SM05Svc {
 		return result;
 	}
 
-//	@Override
-//	public int deleteIoCost(Map<String, String> paramMap) throws Exception {
-//		// ---------------------------------------------------------------
-//		// 첨부 화일 권한체크 시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
-//		// 필수값 : jobType, userId, comonCd
-//		// ---------------------------------------------------------------
-//		List<Map<String, String>> deleteFileList = cm08Svc.selectFileListAll(paramMap);
-//		HashMap<String, String> param = new HashMap<>();
-//		param.put("jobType", "fileDelete");
-//		param.put("userId", paramMap.get("userId"));
-//		if (deleteFileList.size() > 0) {
-//			for (Map<String, String> dtl : deleteFileList) {
-//				// 접근 권한 없으면 Exception 발생
-//				param.put("comonCd", dtl.get("comonCd"));
-//
-//				cm15Svc.selectFileAuthCheck(param);
-//			}
-//		}
-//		// ---------------------------------------------------------------
-//		// 첨부 화일 권한체크 끝
-//		// ---------------------------------------------------------------
-//
-//		int result = sm05Mapper.deleteIoCost(paramMap);
-//
-//		// ---------------------------------------------------------------
-//		// 첨부 화일 처리 시작 (처음 등록시에는 화일 삭제할게 없음)
-//		// ---------------------------------------------------------------
-//		if (deleteFileList.size() > 0) {
-//			for (Map<String, String> deleteDtl : deleteFileList) {
-//				String fileKey = deleteDtl.get("fileKey").toString();
-//				cm08Svc.deleteFile(fileKey);
-//			}
-//		}
-//		// ---------------------------------------------------------------
-//		// 첨부 화일 처리 끝
-//		// ---------------------------------------------------------------
-//		return result;
-//	}
+	@Override
+	public int update_sm05_IoInfo(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) {
+		int result = sm05Mapper.update_sm05_IoInfo(paramMap);
+		
+		return result;
+	}
+	
+	@Override
+	public int delete_sm05M_io(Map<String, String> paramMap) throws Exception {
+		// ---------------------------------------------------------------
+		// 첨부 화일 권한체크 시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
+		// 필수값 : jobType, userId, comonCd
+		// ---------------------------------------------------------------
+		List<Map<String, String>> deleteFileList = cm08Svc.selectFileListAll(paramMap);
+		HashMap<String, String> param = new HashMap<>();
+		param.put("jobType", "fileDelete");
+		param.put("userId", paramMap.get("userId"));
+		if (deleteFileList.size() > 0) {
+			for (Map<String, String> dtl : deleteFileList) {
+				// 접근 권한 없으면 Exception 발생
+				param.put("comonCd", dtl.get("comonCd"));
+
+				cm15Svc.selectFileAuthCheck(param);
+			}
+		}
+		// ---------------------------------------------------------------
+		// 첨부 화일 권한체크 끝
+		// ---------------------------------------------------------------
+
+		int result = sm05Mapper.delete_sm05M_io(paramMap);
+
+		// ---------------------------------------------------------------
+		// 첨부 화일 처리 시작 (처음 등록시에는 화일 삭제할게 없음)
+		// ---------------------------------------------------------------
+		if (deleteFileList.size() > 0) {
+			for (Map<String, String> deleteDtl : deleteFileList) {
+				String fileKey = deleteDtl.get("fileKey").toString();
+				cm08Svc.deleteFile(fileKey);
+			}
+		}
+		// ---------------------------------------------------------------
+		// 첨부 화일 처리 끝
+		// ---------------------------------------------------------------
+		return result;
+	}
+	
+	@Override
+	public int delete_sm05D_io(Map<String, String> paramMap) {
+		int result = sm05Mapper.delete_sm05D_io(paramMap);
+		return result;
+	}
 
 }
