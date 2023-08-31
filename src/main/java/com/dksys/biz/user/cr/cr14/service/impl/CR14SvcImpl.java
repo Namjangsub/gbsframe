7a6355dea761567213ace25c4809afaaad3b2d89
zsjc065 <zenji1106!>
2023-08-31 18:41:07 +0900

    수주현황

---
+++ src/main/java/com/dksys/biz/user/cr/cr14/service/impl/CR14SvcImpl.java
@@ -0,0 +1,49 @@
+package com.dksys.biz.user.cr.cr14.service.impl;
+
+import java.lang.reflect.Type;
+import java.text.Format.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.dksys.biz.user.cr.cr14.mapper.CR14Mapper;
+import com.dksys.biz.user.cr.cr14.service.CR14Svc;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class CR14SvcImpl implements CR14Svc {
+	
+	@Autowired
+	CR14Mapper cr14Mapper;
+	
+	@Autowired
+	CR14Svc cr14Svc;
+
+	// 그리드 카운트
+	@Override
+	public int select_cr14_Count(Map<String, String> paramMap) {
+		return cr14Mapper.select_cr14_Count(paramMap);
+	}
+
+	// 그리드 리스트
+	@Override
+	public List<Map<String, String>> select_cr14_List(Map<String, String> paramMap) {
+		return cr14Mapper.select_cr14_List(paramMap);
+	}
+
+	// @Override
+	// public List<Map<String, Object>> selectPmntmtdCd(Map<String, String> paramMap) {
+	// 	return cr05Mapper.selectPmntmtdCd(paramMap);
+	// }
+}
