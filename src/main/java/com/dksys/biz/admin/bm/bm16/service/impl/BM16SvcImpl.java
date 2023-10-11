484111e5c57c45f36d6a71c3c28809a5c6f5d6fb
sjpark <sjpark@sortech.co.kr>
2023-10-11 10:28:46 +0900

    프로젝트관리 공유결재 첨부파일 업로드

--- src/main/java/com/dksys/biz/admin/bm/bm16/service/impl/BM16SvcImpl.java
+++ src/main/java/com/dksys/biz/admin/bm/bm16/service/impl/BM16SvcImpl.java
@@ -16,6 +16,8 @@ import com.dksys.biz.admin.bm.bm16.mapper.BM16Mapper;
 import com.dksys.biz.admin.bm.bm16.service.BM16Svc;
 import com.dksys.biz.admin.cm.cm08.service.CM08Svc;
 import com.dksys.biz.admin.cm.cm15.service.CM15Svc;
+import com.dksys.biz.user.qm.qm01.mapper.QM01Mapper;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -32,6 +34,9 @@ public class BM16SvcImpl implements BM16Svc {
   
   @Autowired
   CM08Svc cm08Svc;
+  
+  @Autowired
+  QM01Mapper QM01Mapper;
   
   @Override
   public int selectPrjctCount(Map<String, String> paramMap) {
@@ -278,12 +283,198 @@ public class BM16SvcImpl implements BM16Svc {
   @Override
   public int insertPrjctIssue(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
 	  int result = bm16Mapper.insertPrjctIssue(paramMap);
+	  
+	  Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+      Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+      
+      Gson gson = new Gson();	
+      
+      String pgParam = "{\"actionType\":\""+ "U" +"\",";      
+      pgParam += "\"prjctSeq\":\""+ paramMap.get("prjctSeq") +"\"}";
+      
+      Type stringList2 = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+		List<Map<String, String>> sharngArr = gson.fromJson(paramMap.get("rowSharngListArr"), stringList2);
+		if (sharngArr != null && sharngArr.size() > 0 ) {
+			int i = 0;
+	        for (Map<String, String> sharngMap : sharngArr) {
+	            try {	 
+	            	    //sharngMap.put("reqNo", paramMap.get("issNo"));
+	            	    sharngMap.put("reqNo", "1");
+	            	    sharngMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+	            	    sharngMap.put("pgmId", paramMap.get("pgmId"));
+	            	    sharngMap.put("userId", paramMap.get("userId"));
+	            	    sharngMap.put("sanCtnSn",Integer.toString(i+1));
+	            	    sharngMap.put("pgParam", pgParam);
+	                	QM01Mapper.insertWbsSharngList(sharngMap);       		
+	            	i++;
+	            } catch (Exception e) {
+	                System.out.println("error2"+e.getMessage());
+	            }
+	        }
+		}
+
+		//결재
+		Type stringList3 = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+		List<Map<String, String>> approvalArr = gson.fromJson(paramMap.get("rowApprovalListArr"), stringList3);
+		if (approvalArr != null && approvalArr.size() > 0 ) {
+			int i = 0;
+	        for (Map<String, String> approvalMap : approvalArr) {
+	            try {	 
+		            	//approvalMap.put("reqNo", paramMap.get("issNo"));
+	            	    approvalMap.put("reqNo", "1");
+		            	approvalMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+		            	approvalMap.put("pgmId", paramMap.get("pgmId"));
+		            	approvalMap.put("userId", paramMap.get("userId"));
+		            	approvalMap.put("sanCtnSn",Integer.toString(i+1));
+		            	approvalMap.put("pgParam", pgParam);
+	                	QM01Mapper.insertWbsApprovalList(approvalMap);       		
+	                	i++;
+	            } catch (Exception e) {
+	                System.out.println("error2"+e.getMessage());
+	            }
+	        }
+		}
+		
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+	  	//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------  
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+				//접근 권한 없으면 Exception 발생
+				paramMap.put("jobType", "fileUp");
+				cm15Svc.selectFileAuthCheck(paramMap);
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 권한체크  끝 
+		//---------------------------------------------------------------  
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
+		//---------------------------------------------------------------  
+		if (uploadFileList.size() > 0) {
+		    paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+		    paramMap.put("fileTrgtKey", paramMap.get("issFileTrgtKey"));
+		    cm08Svc.uploadFile(paramMap, mRequest);
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 처리  끝 
+		//---------------------------------------------------------------  
+		
 	  return result;
   }
   
   @Override
   public int updatePrjctIssue(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
-	  int result = bm16Mapper.updatePrjctIssue(paramMap);
+	    int result = bm16Mapper.updatePrjctIssue(paramMap);
+	  
+	    Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+	    Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+	    
+	    //---------------------------------------------------------------  
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+	  	//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------  
+	    HashMap<String, String> param = new HashMap<>();
+   	    param.put("userId", paramMap.get("userId"));
+	    param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
+	    
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+				//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
+				param.put("jobType", "fileUp");
+				cm15Svc.selectFileAuthCheck(param);
+		}
+		String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
+		List<String> deleteFileList = Arrays.asList(deleteFileArr);
+	    for(String fileKey : deleteFileList) {  // 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
+			    Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
+				//접근 권한 없으면 Exception 발생
+			    param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
+			    param.put("jobType", "fileDelete");
+				cm15Svc.selectFileAuthCheck(param);
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 권한체크  끝  
+		//---------------------------------------------------------------  
+	    
+	    
+
+		Gson gson = new Gson();					
+
+		String pgParam = "{\"actionType\":\""+ "U" +"\",";      
+	    pgParam += "\"prjctSeq\":\""+ paramMap.get("prjctSeq") +"\"}";
+	    
+		paramMap.put("reqNo", paramMap.get("issNo"));
+		paramMap.put("salesCd", paramMap.get("prjctSeq"));
+		
+		List<Map<String, String>> sharngChk = QM01Mapper.deleteWbsSharngListChk(paramMap); 
+		if (sharngChk.size() > 0) {
+			QM01Mapper.deleteWbsSharngList(paramMap); 
+		}
+		
+		List<Map<String, String>> approvalgChk = QM01Mapper.deleteWbsApprovalListChk(paramMap); 
+		if (approvalgChk.size() > 0) {
+			QM01Mapper.deleteWbsApprovalList(paramMap); 
+		}
+		
+		Type stringList2 = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+		List<Map<String, String>> sharngArr = gson.fromJson(paramMap.get("rowSharngListArr"), stringList2);
+		if (sharngArr != null && sharngArr.size() > 0 ) {
+			int i = 0;
+	        for (Map<String, String> sharngMap : sharngArr) {
+	            try {	 
+	            	    sharngMap.put("reqNo", paramMap.get("issNo"));
+	            	    sharngMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+	            	    sharngMap.put("pgmId", paramMap.get("pgmId"));
+	            	    sharngMap.put("userId", paramMap.get("userId"));
+	            	    sharngMap.put("sanCtnSn",Integer.toString(i+1));
+	            	    sharngMap.put("pgParam", pgParam);
+	                	QM01Mapper.insertWbsSharngList(sharngMap);       		
+	            	i++;
+	            } catch (Exception e) {
+	                System.out.println("error2"+e.getMessage());
+	            }
+	        }
+		}
+
+		//결재
+		Type stringList3 = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+		List<Map<String, String>> approvalArr = gson.fromJson(paramMap.get("rowApprovalListArr"), stringList3);
+		if (approvalArr != null && approvalArr.size() > 0 ) {
+			int i = 0;
+	        for (Map<String, String> approvalMap : approvalArr) {
+	            try {	 
+		            	approvalMap.put("reqNo", paramMap.get("issNo"));
+		            	approvalMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+		            	approvalMap.put("pgmId", paramMap.get("pgmId"));
+		            	approvalMap.put("userId", paramMap.get("userId"));
+		            	approvalMap.put("sanCtnSn",Integer.toString(i+1));
+		            	approvalMap.put("pgParam", pgParam);
+	                	QM01Mapper.insertWbsApprovalList(approvalMap);       		
+	                	i++;
+	            } catch (Exception e) {
+	                System.out.println("error2"+e.getMessage());
+	            }
+	        }
+		}
+		
+	    
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 시작 
+		//---------------------------------------------------------------  
+	    if (uploadFileList.size() > 0) {
+		    paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+		    paramMap.put("fileTrgtKey", paramMap.get("prjctSeq"));
+		    cm08Svc.uploadFile(paramMap, mRequest);
+	    }
+	    
+	    for(String fileKey : deleteFileList) {
+	    	cm08Svc.deleteFile(fileKey);
+	    }
+		//---------------------------------------------------------------  
+		//첨부 화일 처리  끝 
+		//---------------------------------------------------------------  
+	    
 	  return result;
   }
 
@@ -300,6 +491,11 @@ public class BM16SvcImpl implements BM16Svc {
   @Override
   public List<Map<String, Object>> selectPrjctPlanList(Map<String, String> paramMap) {
     return bm16Mapper.selectPrjctPlanList(paramMap);
+  }
+  
+  @Override
+  public Map<String, String> selectTodoIssueInfo(Map<String, String> paramMap) {
+	  return bm16Mapper.selectTodoIssueInfo(paramMap);
   }
   
 }
