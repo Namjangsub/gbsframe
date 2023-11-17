286f4425361cab69e3b25957a40ec5444e34ccbc
panja <coralpanja@gmail.com>
2023-11-17 10:10:47 +0900

    작업일보현황(개인별)화면 개발 11/17

---
+++ src/main/java/com/dksys/biz/user/pm/pm37/PM37Ctr.java
@@ -0,0 +1,49 @@
+package com.dksys.biz.user.pm.pm37;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.dksys.biz.user.pm.pm37.service.PM37Svc;
+import com.dksys.biz.cmn.vo.PaginationInfo;
+import com.dksys.biz.util.MessageUtils;
+
+@Controller
+@RequestMapping("/user/pm/pm37")
+public class PM37Ctr {
+
+  @Autowired
+  MessageUtils messageUtils;
+
+  @Autowired
+  PM37Svc pm37Svc;
+
+  // 작업일보 리스트 조회
+  @PostMapping(value = "/selectDailyWorkMainList")
+  public String selectDailyWorkMainList(@RequestBody Map<String, String> paramMap, ModelMap model) {
+    int totalCnt = pm37Svc.selectDailyWorkCount(paramMap);
+    PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+    model.addAttribute("paginationInfo", paginationInfo);
+    List<Map<String, String>> result = pm37Svc.selectDailyWorkMainList(paramMap);
+    model.addAttribute("result", result);
+    return "jsonView";
+  }
+  	//팀원 검색
+	@PostMapping(value = "/select_all_name")
+	public String select_all_name(@RequestBody Map<String, String> paramMap, ModelMap model) {
+		List<Map<String, String>> result = pm37Svc.select_all_name(paramMap);
+		model.addAttribute("result", result);
+		return "jsonView";
+	}
+}
---
+++ src/main/java/com/dksys/biz/user/pm/pm37/service/impl/PM37SvcImpl.java
@@ -0,0 +1,65 @@
+package com.dksys.biz.user.pm.pm37.service.impl;
+
+import java.lang.reflect.Type;
+import java.text.Format.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.dksys.biz.user.pm.pm37.mapper.PM37Mapper;
+import com.dksys.biz.user.pm.pm37.service.PM37Svc;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class PM37SvcImpl implements PM37Svc{
+	  @Autowired
+	  PM37Mapper pm37Mapper;
+	  
+	  @Override
+	  public int selectDailyWorkCount(Map<String, String> paramMap) {
+	    return pm37Mapper.selectDailyWorkCount(paramMap);
+	  }
+	  
+	@Override
+	  public List<Map<String, String>> selectDailyWorkMainList(Map<String, String> paramMap) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		String[] roleArray = paramMap.get("userName").split(",");
+		String test = "";
+		for (int i = 0; i < roleArray.length; i++) {
+			if ((roleArray.length -1) == i ) {
+				test += "'" + roleArray[i] +"' AS user"+ (i+1);
+			}
+			else {
+				test += "'" + roleArray[i] +"' AS user"+ (i+1) +  ",";
+			}
+		}
+
+		map.put("test", test);
+		map.put("userId", paramMap.get("userId"));
+		map.put("coCd", paramMap.get("coCd"));
+		map.put("reqDtFrom", paramMap.get("reqDtFrom"));
+		map.put("reqDtTo",paramMap.get("reqDtTo"));
+		map.put("salesCd",paramMap.get("salesCd"));
+		map.put("workRptm",paramMap.get("workRptm"));
+		map.put("workRpts",paramMap.get("workRpts"));
+		
+	    return pm37Mapper.selectDailyWorkMainList(map);
+	  }
+	
+	@Override
+	public List<Map<String, String>> select_all_name(Map<String, String> paramMap) {
+		return pm37Mapper.select_all_name(paramMap);
+	}
+}
