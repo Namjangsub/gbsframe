c08d086b8ba415ffe53f3396072febeab58aa940
zsjc065 <zenji1106!>
2023-11-28 20:37:47 +0900

    제품출고 수정

--- src/main/java/com/dksys/biz/user/pm/pm03/service/impl/PM03SvcImpl.java
+++ src/main/java/com/dksys/biz/user/pm/pm03/service/impl/PM03SvcImpl.java
@@ -23,46 +23,44 @@ import com.google.gson.reflect.TypeToken;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class PM03SvcImpl implements PM03Svc {
-
-  @Autowired
-  PM03Mapper pm03Mapper;
-
-  @Autowired
-  CM15Svc cm15Svc;
-
-  @Autowired
-  CM08Svc cm08Svc;
-
-  @Override
-  public int selectDeliveryPageCount(Map<String, String> paramMap) {
-    return pm03Mapper.selectDeliveryPageCount(paramMap);
-  }
-
-  @Override
-  public List<Map<String, String>> selectDeliveryPageList(Map<String, String> paramMap) {
-    return pm03Mapper.selectDeliveryPageList(paramMap);
-  }
-
-  @Override
-  public List<Map<String, String>> selectDeliveryList(Map<String, String> paramMap) {
-    return pm03Mapper.selectDeliveryList(paramMap);
-  }
-
-  @Override
-  public List<Map<String, String>> selectSelesCdList(Map<String, String> paramMap) {
-    return pm03Mapper.selectSelesCdList(paramMap);
-  }
-
-  @Override
-  public Map<String, String> selectDeliveryMastInfo(Map<String, String> paramMap) {
-    return pm03Mapper.selectDeliveryMastInfo(paramMap);
-  }
-
-  @Override
-  public int insertDeliveryMast(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
-
-	    Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
-	    Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+	@Autowired
+	PM03Mapper pm03Mapper;
+	
+	@Autowired
+	CM15Svc cm15Svc;
+	
+	@Autowired
+	CM08Svc cm08Svc;
+	
+	@Override
+	public int selectDeliveryPageCount(Map<String, String> paramMap) {
+		return pm03Mapper.selectDeliveryPageCount(paramMap);
+	}
+	
+	@Override
+	public List<Map<String, String>> selectDeliveryPageList(Map<String, String> paramMap) {
+		return pm03Mapper.selectDeliveryPageList(paramMap);
+	}
+	
+	@Override
+	public List<Map<String, String>> selectDeliveryList(Map<String, String> paramMap) {
+		return pm03Mapper.selectDeliveryList(paramMap);
+	}
+	
+	@Override
+	public List<Map<String, String>> selectSelesCdList(Map<String, String> paramMap) {
+		return pm03Mapper.selectSelesCdList(paramMap);
+	}
+	
+	@Override
+	public Map<String, String> selectDeliveryMastInfo(Map<String, String> paramMap) {
+		return pm03Mapper.selectDeliveryMastInfo(paramMap);
+	}
+	
+	@Override
+	public int insertDeliveryMast(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+		Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+		Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
 
 		//---------------------------------------------------------------
 		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
@@ -81,23 +79,24 @@ public class PM03SvcImpl implements PM03Svc {
 		paramMap.put("outNo", outNo);
 		int result = pm03Mapper.insertDeliveryMast(paramMap);
 		//pm03Mapper.deleteLgistSalesCdAll(paramMap);
-
-	    List<Map<String, String>> salesCdList = gsonDtl.fromJson(paramMap.get("salesCdArr"), dtlMap);
+		
+		// List<Map<String, String>> salesCdList = gsonDtl.fromJson(paramMap.get("salesCdArr"), dtlMap);
+		List<Map<String, String>> salesCdList = gsonDtl.fromJson(paramMap.get("detailArr"), dtlMap);
 	    for (Map<String, String> dtl : salesCdList) {
 	    	dtl.put("outNo", outNo);
 	    	dtl.put("userId", paramMap.get("userId"));
 	    	dtl.put("pgmId", paramMap.get("pgmId"));
-	    	pm03Mapper.insertLgistSalesCd(dtl);
-	    	/*
-	    	String dtaChk = dtl.get("dtaChk").toString();
-	    	if ("I".equals(dtaChk)) {
-	    		pm03Mapper.insertLgistSalesCd(dtl);
-	    	} else if ("U".equals(dtaChk)) {
-	    		pm03Mapper.updateLgistSalesCd(dtl);
-	    	} else if ("D".equals(dtaChk)) {
-	    		pm03Mapper.deleteLgistSalesCd(dtl);
-	    	}
-	    	*/
+			pm03Mapper.insertLgistSalesCd(dtl);
+			
+	    	// pm03Mapper.insertLgistSalesCd(dtl);
+	    	// String dtaChk = dtl.get("dtaChk").toString();
+	    	// if ("I".equals(dtaChk)) {
+	    	// 	pm03Mapper.insertLgistSalesCd(dtl);
+	    	// } else if ("U".equals(dtaChk)) {
+	    	// 	pm03Mapper.updateLgistSalesCd(dtl);
+	    	// } else if ("D".equals(dtaChk)) {
+	    	// 	pm03Mapper.deleteLgistSalesCd(dtl);
+	    	// }
 	    }
 
 		//---------------------------------------------------------------
@@ -113,87 +112,88 @@ public class PM03SvcImpl implements PM03Svc {
 		//---------------------------------------------------------------
 
 	    return result;
-  }
-  @Override
-  public int updateDeliveryMast(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
-//	Gson gson = new Gson();
-	Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
-	Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
-
-	//---------------------------------------------------------------
-	//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
-  	//   필수값 :  jobType, userId, comonCd
-	//---------------------------------------------------------------
-    HashMap<String, String> param = new HashMap<>();
-    param.put("userId", paramMap.get("userId"));
-    param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
-
-	List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
-	if (uploadFileList.size() > 0) {
-			//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
-			param.put("jobType", "fileUp");
-			cm15Svc.selectFileAuthCheck(param);
 	}
-	String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
-	List<String> deleteFileList = Arrays.asList(deleteFileArr);
-    for(String fileKey : deleteFileList) {  // 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
-		    Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
-			//접근 권한 없으면 Exception 발생
-		    param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
-		    param.put("jobType", "fileDelete");
-			cm15Svc.selectFileAuthCheck(param);
+	
+	@Override
+	public int updateDeliveryMast(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
+		// Gson gson = new Gson();
+		Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+		Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
+
+		//---------------------------------------------------------------
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+		//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------
+		HashMap<String, String> param = new HashMap<>();
+		param.put("userId", paramMap.get("userId"));
+		param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
+
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+				//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
+				param.put("jobType", "fileUp");
+				cm15Svc.selectFileAuthCheck(param);
+		}
+		String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
+		List<String> deleteFileList = Arrays.asList(deleteFileArr);
+		for(String fileKey : deleteFileList) {  // 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
+				Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
+				//접근 권한 없으면 Exception 발생
+				param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
+				param.put("jobType", "fileDelete");
+				cm15Svc.selectFileAuthCheck(param);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 권한체크  끝
+		//---------------------------------------------------------------
+
+		int result = pm03Mapper.updateDeliveryMast(paramMap);
+			//pm03Mapper.deleteLgistSalesCdAll(paramMap);
+
+
+		List<Map<String, String>> salesCdList = gsonDtl.fromJson(paramMap.get("salesCdArr"), dtlMap);
+		for (Map<String, String> dtl : salesCdList) {
+			dtl.put("userId", paramMap.get("userId"));
+			dtl.put("pgmId", paramMap.get("pgmId"));
+			pm03Mapper.updateLgistSalesCd(dtl);
+			//String dtaChk = dtl.get("dtaChk").toString();
+			/* "dtaChk" 값을 확인하여
+			* "I"인 경우 pm03Mapper.insertLgistSalesCd(dtl)을 호출하여 프로젝트 세부정보를 삽입하고,
+			* "U"인 경우 pm03Mapper.updateLgistSalesCd(dtl)을 호출하여 프로젝트 세부정보를 업데이트하고,
+			* "D"인 경우 * pm03Mapper.deleteLgistSalesCd(dtl)을 호출하여 프로젝트 세부정보를 삭제.		 */
+			/*
+			if ("I".equals(dtaChk)) {
+				pm03Mapper.insertLgistSalesCd(dtl);
+			} else if ("U".equals(dtaChk)) {
+				pm03Mapper.updateLgistSalesCd(dtl);
+			} else if ("D".equals(dtaChk)) {
+				pm03Mapper.deleteLgistSalesCd(dtl);
+			}
+			*/
+		}
+
+		//---------------------------------------------------------------
+		//첨부 화일 처리 시작
+		//---------------------------------------------------------------
+		if (uploadFileList.size() > 0) {
+			paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+			paramMap.put("fileTrgtKey", paramMap.get("fileTrgtKey"));
+			cm08Svc.uploadFile(paramMap, mRequest);
+		}
+
+		for(String fileKey : deleteFileList) {
+			cm08Svc.deleteFile(fileKey);
+		}
+		//---------------------------------------------------------------
+		//첨부 화일 처리  끝
+		//---------------------------------------------------------------
+
+		return result;
 	}
-	//---------------------------------------------------------------
-	//첨부 화일 권한체크  끝
-	//---------------------------------------------------------------
-
-	int result = pm03Mapper.updateDeliveryMast(paramMap);
-		//pm03Mapper.deleteLgistSalesCdAll(paramMap);
-
-
-    List<Map<String, String>> salesCdList = gsonDtl.fromJson(paramMap.get("salesCdArr"), dtlMap);
-    for (Map<String, String> dtl : salesCdList) {
-    	dtl.put("userId", paramMap.get("userId"));
-    	dtl.put("pgmId", paramMap.get("pgmId"));
-    	pm03Mapper.updateLgistSalesCd(dtl);
-    	//String dtaChk = dtl.get("dtaChk").toString();
-    	/* "dtaChk" 값을 확인하여
-    	 * "I"인 경우 pm03Mapper.insertLgistSalesCd(dtl)을 호출하여 프로젝트 세부정보를 삽입하고,
-    	 * "U"인 경우 pm03Mapper.updateLgistSalesCd(dtl)을 호출하여 프로젝트 세부정보를 업데이트하고,
-    	 * "D"인 경우 * pm03Mapper.deleteLgistSalesCd(dtl)을 호출하여 프로젝트 세부정보를 삭제.		 */
-    	/*
-    	if ("I".equals(dtaChk)) {
-    		pm03Mapper.insertLgistSalesCd(dtl);
-    	} else if ("U".equals(dtaChk)) {
-    		pm03Mapper.updateLgistSalesCd(dtl);
-    	} else if ("D".equals(dtaChk)) {
-    		pm03Mapper.deleteLgistSalesCd(dtl);
-    	}
-    	*/
-    }
-
-	//---------------------------------------------------------------
-	//첨부 화일 처리 시작
-	//---------------------------------------------------------------
-    if (uploadFileList.size() > 0) {
-	    paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
-	    paramMap.put("fileTrgtKey", paramMap.get("fileTrgtKey"));
-	    cm08Svc.uploadFile(paramMap, mRequest);
-    }
-
-    for(String fileKey : deleteFileList) {
-    	cm08Svc.deleteFile(fileKey);
-    }
-	//---------------------------------------------------------------
-	//첨부 화일 처리  끝
-	//---------------------------------------------------------------
-
-    return result;
-  }
-
-  @Override
-  public int deleteDeliveryMast(Map<String, String> paramMap) throws Exception {
-	    //---------------------------------------------------------------
+	
+	@Override
+	public int deleteDeliveryMast(Map<String, String> paramMap) throws Exception {
+		//---------------------------------------------------------------
 		//첨부 화일 권한체크  시작 -->삭제 권한 없으면 Exception, 관련 화일 전체 체크
 	  	//   필수값 :  jobType, userId, comonCd
 		//---------------------------------------------------------------
@@ -212,9 +212,8 @@ public class PM03SvcImpl implements PM03Svc {
 		//---------------------------------------------------------------
 		//첨부 화일 권한체크 끝
 		//---------------------------------------------------------------
-
-	  int result = pm03Mapper.deleteLgistSalesCdAll(paramMap);
-	  result = pm03Mapper.deleteDeliveryMast(paramMap);
+		int result = pm03Mapper.deleteLgistSalesCdAll(paramMap);
+		result = pm03Mapper.deleteDeliveryMast(paramMap);
 
 		//---------------------------------------------------------------
 		//첨부 화일 처리 시작  (처음 등록시에는 화일 삭제할게 없음)
@@ -229,6 +228,5 @@ public class PM03SvcImpl implements PM03Svc {
 		//첨부 화일 처리  끝
 		//---------------------------------------------------------------
 	    return result;
-  }
-
+	}
 }
