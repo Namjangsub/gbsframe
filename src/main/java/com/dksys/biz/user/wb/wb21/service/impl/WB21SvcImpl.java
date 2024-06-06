188e1e1ab47a84ec4919abea22229da35eb64ea8
Namjangsub <firefly7@korea.com>
2024-06-07 07:15:36 +0900

    과제등록 첨부파일 기능 추가

--- src/main/java/com/dksys/biz/user/wb/wb21/service/impl/WB21SvcImpl.java
+++ src/main/java/com/dksys/biz/user/wb/wb21/service/impl/WB21SvcImpl.java
@@ -2,6 +2,7 @@ package com.dksys.biz.user.wb.wb21.service.impl;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.dksys.biz.admin.cm.cm08.service.CM08Svc;
+import com.dksys.biz.admin.cm.cm15.service.CM15Svc;
 import com.dksys.biz.user.qm.qm01.mapper.QM01Mapper;
 import com.dksys.biz.user.wb.wb21.mapper.WB21Mapper;
 import com.dksys.biz.user.wb.wb21.service.WB21Svc;
@@ -26,6 +29,11 @@ public class WB21SvcImpl implements WB21Svc {
     @Autowired
     WB21Mapper wb21Mapper;
 
+    @Autowired
+    CM15Svc cm15Svc;
+
+    @Autowired
+    CM08Svc cm08Svc;
     
     @Autowired
     QM01Mapper QM01Mapper;
@@ -183,6 +191,37 @@ public class WB21SvcImpl implements WB21Svc {
 	@Override
 	public int sjUpdate(Map<String, String> paramMap, MultipartHttpServletRequest mRequest) throws Exception {
 		Gson gson = new Gson(); 
+		Gson gsonDtl = new GsonBuilder().disableHtmlEscaping().create();
+		Type dtlMap = new TypeToken<ArrayList<Map<String, String>>>(){}.getType();
+
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 권한체크 시작 -->파일 업로드, 삭제 권한 없으면 Exception 처리 됨
+	  	//   필수값 :  jobType, userId, comonCd
+		//---------------------------------------------------------------  
+	    HashMap<String, String> param = new HashMap<>();
+	    param.put("userId", paramMap.get("userId"));
+	    param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
+	    
+		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
+		if (uploadFileList.size() > 0) {
+				//접근 권한 없으면 Exception 발생 (jobType, userId, comonCd 3개 필수값 필요)
+				param.put("jobType", "fileUp");
+				cm15Svc.selectFileAuthCheck(param);
+		}
+		String[] deleteFileArr = gsonDtl.fromJson(paramMap.get("deleteFileArr"), String[].class);
+		List<String> deleteFileList = Arrays.asList(deleteFileArr);
+	    for(String fileKey : deleteFileList) {  // 삭제할 파일 하나씩 점검 필요(전체 목록에서 삭제 선택시 필요함)
+			    Map<String, String> fileInfo = cm08Svc.selectFileInfo(fileKey);
+				//접근 권한 없으면 Exception 발생
+			    param.put("comonCd", fileInfo.get("comonCd"));  //삭제할 파일이 보관된 저장 위치 정보
+			    param.put("jobType", "fileDelete");
+				cm15Svc.selectFileAuthCheck(param);
+		}
+		//---------------------------------------------------------------  
+		//첨부 화일 권한체크  끝 
+		//---------------------------------------------------------------  
+		
+		
 		Type stringList = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
 		List<Map<String, String>> sharngArr = gson.fromJson(paramMap.get("rowListArr"), stringList);
 		if (sharngArr != null && sharngArr.size() > 0 ) {
@@ -196,6 +235,23 @@ public class WB21SvcImpl implements WB21Svc {
 		}
 
 		int result = wb21Mapper.sjUpdate(paramMap);
+	    
+		//---------------------------------------------------------------  
+		//첨부 화일 처리 시작 
+		//---------------------------------------------------------------  
+	    if (uploadFileList.size() > 0) {
+		    paramMap.put("fileTrgtTyp", paramMap.get("pgmId"));
+		    paramMap.put("fileTrgtKey", paramMap.get("sjNo"));
+		    cm08Svc.uploadFile(paramMap, mRequest);
+	    }
+	    
+	    for(String fileKey : deleteFileList) {
+	    	cm08Svc.deleteFile(fileKey);
+	    }
+		//---------------------------------------------------------------  
+		//첨부 화일 처리  끝 
+		//---------------------------------------------------------------  
+	    
 	    return result;
 	}
 	
