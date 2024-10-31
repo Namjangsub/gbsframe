bca692bdc9a687f37de7ae4fa88158cbd32913e7
Namjangsub <firefly7@korea.com>
2024-10-31 11:09:45 +0900

    이전버젼 비교시 현재자료에도 변동부분 색표시함

--- src/main/java/com/dksys/biz/user/cr/cr14/CR14Ctr.java
+++ src/main/java/com/dksys/biz/user/cr/cr14/CR14Ctr.java
@@ -1,22 +1,17 @@
 package com.dksys.biz.user.cr.cr14;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import com.dksys.biz.user.cr.cr14.service.CR14Svc;
 import com.dksys.biz.cmn.vo.PaginationInfo;
+import com.dksys.biz.user.cr.cr14.service.CR14Svc;
 import com.dksys.biz.util.MessageUtils;
 
 @Controller
@@ -29,12 +24,12 @@ public class CR14Ctr {
     @Autowired
     CR14Svc cr14svc;
 
-    //리스트 조회
+    // 리스트 조회
 	@PostMapping(value = "/select_cr14_List")
 	public String select_cr14_List(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_cr14_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+        int totalCnt = cr14svc.select_cr14_Count(paramMap);
+        PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+        model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_cr14_List(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
@@ -43,9 +38,9 @@ public class CR14Ctr {
 	//POPUP 리스트 조회
 	@PostMapping(value = "/select_modal_List1")
 	public String select_modal_List1(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_modal_List1_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+//		int totalCnt = cr14svc.select_modal_List1_Count(paramMap);
+//		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+//		model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_modal_List1(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
@@ -53,9 +48,9 @@ public class CR14Ctr {
 
 	@PostMapping(value = "/select_modal_List2")
 	public String select_modal_List2(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_modal_List2_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+//		int totalCnt = cr14svc.select_modal_List2_Count(paramMap);
+//		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+//		model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_modal_List2(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
@@ -63,9 +58,9 @@ public class CR14Ctr {
 
 	@PostMapping(value = "/select_modal_List3")
 	public String select_modal_List3(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_modal_List3_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+//		int totalCnt = cr14svc.select_modal_List3_Count(paramMap);
+//		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+//		model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_modal_List3(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
@@ -73,9 +68,9 @@ public class CR14Ctr {
 
 	@PostMapping(value = "/select_modal_List4")
 	public String select_modal_List4(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_modal_List4_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+//		int totalCnt = cr14svc.select_modal_List4_Count(paramMap);
+//		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+//		model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_modal_List4(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
@@ -83,9 +78,9 @@ public class CR14Ctr {
 
 	@PostMapping(value = "/select_modal_List5")
 	public String select_modal_List5(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_modal_List5_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+//		int totalCnt = cr14svc.select_modal_List5_Count(paramMap);
+//		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+//		model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_modal_List5(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
@@ -93,9 +88,9 @@ public class CR14Ctr {
 
 	@PostMapping(value = "/select_modal_List6")
 	public String select_modal_List6(@RequestBody Map<String, String> paramMap, ModelMap model) {
-		int totalCnt = cr14svc.select_modal_List6_Count(paramMap);
-		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
-		model.addAttribute("paginationInfo", paginationInfo);
+//		int totalCnt = cr14svc.select_modal_List6_Count(paramMap);
+//		PaginationInfo paginationInfo = new PaginationInfo(paramMap, totalCnt);
+//		model.addAttribute("paginationInfo", paginationInfo);
 		List<Map<String, String>> result = cr14svc.select_modal_List6(paramMap);
 		model.addAttribute("result", result);
 		return "jsonView";
