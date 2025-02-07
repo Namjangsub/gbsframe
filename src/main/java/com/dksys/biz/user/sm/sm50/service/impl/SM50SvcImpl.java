44f9b420ee3d827df59e31cb15f7cd516bfd6286
Namjangsub <firefly7@korea.com>
2025-02-07 15:18:56 +0900

    BOM에 등록된 소재,규격,사이즈,비고 그대로 보여주기

--- src/main/java/com/dksys/biz/user/sm/sm50/service/impl/SM50SvcImpl.java
+++ src/main/java/com/dksys/biz/user/sm/sm50/service/impl/SM50SvcImpl.java
@@ -20,63 +20,63 @@ import com.google.gson.reflect.TypeToken;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class SM50SvcImpl implements SM50Svc {
-	
-	@Autowired
-	SM50Mapper bm50Mapper;
-	
-	@Autowired
-	SM50Svc sm50Svc;
+    
+    @Autowired
+    SM50Mapper bm50Mapper;
+    
+    @Autowired
+    SM50Svc sm50Svc;
 
-	
-	@Override
-	public List<Map<String, String>> selectBomCostTreeList(Map<String, String> paramMap) {
+    
+    @Override
+    public List<Map<String, String>> selectBomCostTreeList(Map<String, String> paramMap) {
 
-		//Top SalesCd 정보 가져오기
-//    	Map<String, String> pcostInfo = sm50Svc.selectBomTrgtPchsPcostInfo(paramMap);
-		
-	    // BOM비용 산정 프로시져 호출
-		bm50Mapper.callBomTempUpd(paramMap);
-    	
-		return bm50Mapper.selectBomCostTreeList(paramMap);
-	}
+        //Top SalesCd 정보 가져오기
+//      Map<String, String> pcostInfo = sm50Svc.selectBomTrgtPchsPcostInfo(paramMap);
+        
+        // BOM비용 산정 프로시져 호출
+        bm50Mapper.callBomTempUpd(paramMap);
+        
+        return bm50Mapper.selectBomCostTreeList(paramMap);
+    }
 
-	@Override
-	public List<Map<String, String>> selectBomlevelList(Map<String, String> paramMap) {
-		return bm50Mapper.selectBomlevelList(paramMap);
-	}
-	
-	@Override
-	public List<Map<String, String>> selectBomAllCostList(Map<String, String> paramMap) {
-	    // BOM비용 산정 프로시져 호출
-//    	Map<String, String> resultTemp = sm50Svc.callBomTempUpd(paramMap);
-    	
-		return bm50Mapper.selectBomAllCostList(paramMap);
-	}
+    @Override
+    public List<Map<String, String>> selectBomlevelList(Map<String, String> paramMap) {
+        return bm50Mapper.selectBomlevelList(paramMap);
+    }
+    
+    @Override
+    public List<Map<String, String>> selectBomAllCostList(Map<String, String> paramMap) {
+        // BOM비용 산정 프로시져 호출
+//      Map<String, String> resultTemp = sm50Svc.callBomTempUpd(paramMap);
+        
+        return bm50Mapper.selectBomAllCostList(paramMap);
+    }
 
-	
-	
-	@Override
-	public List<Map<String, String>> selectBomAllLevelTempList(Map<String, String> paramMap) {
-		return bm50Mapper.selectBomAllLevelTempList(paramMap);
-	}
-	
-	
-	@Override
-	public Map<String, String> callBomTempUpd(Map<String, String> paramMap) {
-		bm50Mapper.callBomTempUpd(paramMap);
-		return paramMap;
-	}
-	
-	@Override
-	public List<Map<String, String>> selectBomAllEnterList(Map<String, String> paramMap) {
-		return bm50Mapper.selectBomAllEnterList(paramMap);
-	}
-	
+    
+    
+    @Override
+    public List<Map<String, String>> selectBomAllLevelTempList(Map<String, String> paramMap) {
+        return bm50Mapper.selectBomAllLevelTempList(paramMap);
+    }
+    
+    
+    @Override
+    public Map<String, String> callBomTempUpd(Map<String, String> paramMap) {
+        bm50Mapper.callBomTempUpd(paramMap);
+        return paramMap;
+    }
+    
+    @Override
+    public List<Map<String, String>> selectBomAllEnterList(Map<String, String> paramMap) {
+        return bm50Mapper.selectBomAllEnterList(paramMap);
+    }
+    
 
-	
-	@Override
-	public Map<String, String> selectBomTrgtPchsPcostInfo(Map<String, String> paramMap) {
-		return bm50Mapper.selectBomTrgtPchsPcostInfo(paramMap);
-	}
+    
+    @Override
+    public Map<String, String> selectBomTrgtPchsPcostInfo(Map<String, String> paramMap) {
+        return bm50Mapper.selectBomTrgtPchsPcostInfo(paramMap);
+    }
 
 }
