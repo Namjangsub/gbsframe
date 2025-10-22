d99a3c4bc42e82b4d881013371e7952c2ae3297a
Namjangsub <firefly7@korea.com>
2025-10-23 07:49:19 +0900

    코드 오류 수정

--- src/main/java/com/dksys/biz/user/sm/sm03/service/impl/SM03SvcImpl.java
+++ src/main/java/com/dksys/biz/user/sm/sm03/service/impl/SM03SvcImpl.java
@@ -96,6 +96,7 @@ public class SM03SvcImpl implements SM03Svc {
 		//---------------------------------------------------------------  
 	    HashMap<String, String> param = new HashMap<>();
 	    param.put("userId", paramMap.get("userId"));
+	    param.put("coCd", paramMap.get("coCd"));
 	    param.put("comonCd", paramMap.get("comonCd"));  //프로트엔드에 넘어온 화일 저장 위치 정보
 	    
 		List<Map<String, String>> uploadFileList = gsonDtl.fromJson(paramMap.get("uploadFileArr"), dtlMap);
